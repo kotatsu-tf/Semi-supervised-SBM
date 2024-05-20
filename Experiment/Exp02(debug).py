6cccf548cd0268d4c00e3febfb671609cffc82bd
kotatsu-tf <tatsuya.tf12@gmail.com>
2024-05-20 04:35:49 +0000

    fixed

---
+++ Experiment/Exp02(debug).py
@@ -0,0 +1,13 @@
+# 人工データを用いたS4BMの動作実験
+import numpy as np
+from my_models import S4BM
+
+# CSVファイルのパスを指定
+file_path = 'data/input/artificial_data_for_debug.csv'
+# CSVファイルを読み込んでNumPy配列に変換
+data = np.loadtxt(file_path, delimiter=',')
+categories = [[],[]]
+
+model = S4BM(max_iter=5, n_blocks=2, random_state=102)
+cluster = model.fit_transform(data, categories)
+print(cluster)
