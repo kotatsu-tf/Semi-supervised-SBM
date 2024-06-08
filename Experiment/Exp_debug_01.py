8f5a091efc10a5214c762a93d324b169d386a757
kotatsu-tf <tatsuya.tf12@gmail.com>
2024-06-08 08:57:07 +0000

    exp

--- Experiment/Exp_debug_01.py
+++ Experiment/Exp_debug_01.py
@@ -8,7 +8,7 @@ file_path = 'data/input/artificial_data_debug2a.csv'
 data = np.loadtxt(file_path, delimiter=',')
 categories = [[],[]]
 
-model = S4BM(max_iter=50, n_blocks=2, random_state=103, is_show_params_history=True)
+model = S4BM(max_iter=30, n_blocks=2, random_state=107, is_show_params_history=True)
 cluster = model.fit_transform(data, categories)
 print("------------------------Result------------------------")
 print("model=" + str(model))
