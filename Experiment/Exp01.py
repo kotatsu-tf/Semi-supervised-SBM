f9f4a3a3dd05269dfb1fbb83adc9b135c40226dd
kotatsu-tf <tatsuya.tf12@gmail.com>
2024-08-15 13:06:00 +0000

    gammaの更新関数の修正

--- Experiment/Exp01.py
+++ Experiment/Exp01.py
@@ -9,7 +9,7 @@ file_path = 'data/input/artificial_data_1.csv'
 data = np.loadtxt(file_path, delimiter=',')
 categories=([64, 65, 66, 67], [0, 1, 2, 3], [32, 33, 34, 35], [96, 97, 98, 99])
 
-model = PrMethod(max_iter=10, n_blocks=4, random_state=99)
+model = S4BM(max_iter=10, n_blocks=4, random_state=99)
 cluster = model.fit_transform(data, categories)
 print("------------------------Result------------------------")
 print("model=" + str(model))
